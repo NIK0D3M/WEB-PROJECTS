af6ae5164481307189e5d3aa1aa001b6cc650467
NIK0D3M <liszkanikodem13@gmail.com>
2025-01-12 13:34:25 +0100

    Edit javascript to repair hamburger
    Edit javascript to work properly with hamburger

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const hamburger = document.querySelector('.hamburger-menu');
-const sideMenu = document.querySelector('.side-menu');
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger-menu');
+  const sideMenu = document.querySelector('.side-menu');
+  const menuLinks = document.querySelectorAll('.side-menu a');
 
-hamburger.addEventListener('click', () => {
-  if (sideMenu.style.right === '0px') {
-    sideMenu.style.right = '-100%';
-  } else {
-    sideMenu.style.right = '0';
-  }
+  const toggleMenu = () => {
+    if (sideMenu.style.right === '0px') {
+      sideMenu.style.right = '-100%';
+    } else {
+      sideMenu.style.right = '0';
+    }
+  };
+
+  hamburger.addEventListener('click', toggleMenu);
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      sideMenu.style.right = '-100%';
+    });
+  });
 });
